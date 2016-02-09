36d03d112159bc9a64a7d1e1ce34f0955be8aec1
tchin94 <tchin1994@gmail.com>
2016-02-09 00:33:49 -0800

    added date slider and updated layout

--- public/chordchart.js
+++ public/chordchart.js
@@ -1,53 +1,55 @@
-var matrix = [
-  [11975,  5871, 8916, 2868],
-  [ 1951, 10048, 2060, 6171],
-  [ 8010, 16145, 8090, 8045],
-  [ 1013,   990,  940, 6907]
-];
+$(document).ready(function() {
+  var matrix = [
+    [11975,  5871, 8916, 2868],
+    [ 1951, 10048, 2060, 6171],
+    [ 8010, 16145, 8090, 8045],
+    [ 1013,   990,  940, 6907]
+  ];
 
-var chord = d3.layout.chord()
-    .sortSubgroups(d3.descending)
-    .matrix(matrix);
+  var chord = d3.layout.chord()
+      .sortSubgroups(d3.descending)
+      .matrix(matrix);
 
-var width = 960,
-    height = 720,
-    innerRadius = 150,
-    outerRadius = innerRadius * 1.1;
+  var width = 960,
+      height = 720,
+      innerRadius = 150,
+      outerRadius = innerRadius * 1.1;
 
-var fill = d3.scale.ordinal()
-    .domain(d3.range(4))
-    .range(["#000000", "#FFDD89", "#957244", "#F26223"]);
+  var fill = d3.scale.ordinal()
+      .domain(d3.range(4))
+      .range(["#000000", "#FFDD89", "#957244", "#F26223"]);
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+  var svg = d3.select("#chart").append("svg")
+      .attr("width", width)
+      .attr("height", height)
+    .append("g")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-svg.append("g").selectAll("path")
-    .data(chord.groups)
-  .enter().append("path")
-    .style("fill", function(d) { return fill(d.index); })
-    .style("stroke", function(d) { return fill(d.index); })
-    .attr("d", d3.svg.arc().innerRadius(innerRadius).outerRadius(function(d) { return outerRadius + Math.random() * 100}))
-    .on("mouseover", fade(.1))
-    .on("mouseout", fade(1));
+  svg.append("g").selectAll("path")
+      .data(chord.groups)
+    .enter().append("path")
+      .style("fill", function(d) { return fill(d.index); })
+      .style("stroke", function(d) { return fill(d.index); })
+      .attr("d", d3.svg.arc().innerRadius(innerRadius).outerRadius(function(d) { return outerRadius + Math.random() * 100}))
+      .on("mouseover", fade(.1))
+      .on("mouseout", fade(1));
 
-svg.append("g")
-    .attr("class", "chord")
-  .selectAll("path")
-    .data(chord.chords)
-  .enter().append("path")
-    .attr("d", d3.svg.chord().radius(innerRadius))
-    .style("fill", function(d) { return fill(d.target.index); })
-    .style("opacity", 1);
+  svg.append("g")
+      .attr("class", "chord")
+    .selectAll("path")
+      .data(chord.chords)
+    .enter().append("path")
+      .attr("d", d3.svg.chord().radius(innerRadius))
+      .style("fill", function(d) { return fill(d.target.index); })
+      .style("opacity", 1);
 
-// Returns an event handler for fading a given chord group.
-function fade(opacity) {
-  return function(g, i) {
-    svg.selectAll(".chord path")
-        .filter(function(d) { return d.source.index != i && d.target.index != i; })
-      .transition()
-        .style("opacity", opacity);
-  };
-}
+  // Returns an event handler for fading a given chord group.
+  function fade(opacity) {
+    return function(g, i) {
+      svg.selectAll(".chord path")
+          .filter(function(d) { return d.source.index != i && d.target.index != i; })
+        .transition()
+          .style("opacity", opacity);
+    };
+  }
+});
---
+++ public/ui.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	
+//date slider======================================
+	$("#date-slider").dateRangeSlider({
+		defaultValues:{
+			min: new Date(2013,0,1),
+			max: new Date(2014,11,31)},
+		bounds: {
+			min: new Date(2013,0,1),
+			max: new Date(2014,11,31)},
+		arrows: false});
+
+	$("#date-slider").on("valuesChanging", function(e, data) {
+		console.log("min date: " + data.values.min + " max date: " + data.values.max);
+	});
+});	
