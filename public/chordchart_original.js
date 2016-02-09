bf4472130f25a313d53f915a3f749c94f6b063d2
tchin94 <tchin1994@gmail.com>
2016-02-08 19:29:20 -0800

    simplify and added height

--- public/chordchart_original.js
+++ public/chordchart_original.js
@@ -6,12 +6,13 @@ var matrix = [
 ];
 
 var chord = d3.layout.chord()
+    .padding(.05)
     .sortSubgroups(d3.descending)
     .matrix(matrix);
 
 var width = 960,
-    height = 720,
-    innerRadius = 300,
+    height = 500,
+    innerRadius = Math.min(width, height) * .41,
     outerRadius = innerRadius * 1.1;
 
 var fill = d3.scale.ordinal()
@@ -29,9 +30,33 @@ svg.append("g").selectAll("path")
   .enter().append("path")
     .style("fill", function(d) { return fill(d.index); })
     .style("stroke", function(d) { return fill(d.index); })
-    .attr("d", d3.svg.arc().innerRadius(innerRadius).outerRadius(function(d) { return outerRadius + Math.random() * 100}))
+    .attr("d", d3.svg.arc().innerRadius(innerRadius).outerRadius(outerRadius))
     .on("mouseover", fade(.1))
     .on("mouseout", fade(1));
+
+var ticks = svg.append("g").selectAll("g")
+    .data(chord.groups)
+  .enter().append("g").selectAll("g")
+    .data(groupTicks)
+  .enter().append("g")
+    .attr("transform", function(d) {
+      return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")"
+          + "translate(" + outerRadius + ",0)";
+    });
+
+ticks.append("line")
+    .attr("x1", 1)
+    .attr("y1", 0)
+    .attr("x2", 5)
+    .attr("y2", 0)
+    .style("stroke", "#000");
+
+ticks.append("text")
+    .attr("x", 8)
+    .attr("dy", ".35em")
+    .attr("transform", function(d) { return d.angle > Math.PI ? "rotate(180)translate(-16)" : null; })
+    .style("text-anchor", function(d) { return d.angle > Math.PI ? "end" : null; })
+    .text(function(d) { return d.label; });
 
 svg.append("g")
     .attr("class", "chord")
@@ -41,6 +66,17 @@ svg.append("g")
     .attr("d", d3.svg.chord().radius(innerRadius))
     .style("fill", function(d) { return fill(d.target.index); })
     .style("opacity", 1);
+
+// Returns an array of tick angles and labels, given a group.
+function groupTicks(d) {
+  var k = (d.endAngle - d.startAngle) / d.value;
+  return d3.range(0, d.value, 1000).map(function(v, i) {
+    return {
+      angle: v * k + d.startAngle,
+      label: i % 5 ? null : v / 1000 + "k"
+    };
+  });
+}
 
 // Returns an event handler for fading a given chord group.
 function fade(opacity) {
