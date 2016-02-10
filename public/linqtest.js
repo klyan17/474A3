76ee8ce00b295b4495be4da83780cc4decf8294a
tchin94 <tchin1994@gmail.com>
2016-02-10 00:36:48 -0800

    testing linq

--- public/linqtest.js
+++ public/linqtest.js
@@ -1,3 +1,5 @@
+var movielist
+
 $(document).ready(function() {
 	//parse data
 	var datapath = "movies-2014.csv";
@@ -9,18 +11,24 @@ $(document).ready(function() {
 		
 	});
 
-	var movielist = Papa.parse(csvfile.responseText, {
+	movielist = Papa.parse(csvfile.responseText, {
 					    header: true,
 					    dynamicTyping: true,
 					}).data;
 	console.log(movielist);
 
 	//linq edit
-	// var queryResult = Enumerable.From(movielist)
-	// 	.Where("$data.distributor = 'Walt Disney'")
-	// 	.ToArray();
 
 	var a = movielist
-	.where(function(x) { return x.distributor == "Walt Disney"});
+	.where(function(x) { return filter1(x)})
+	.orderBy(function(x) { return x.distributor})
+	.take(20);
 	console.log(a);
+
+	function filter1(x) {
+		if (x.distributor != null) {
+			return x.distributor.length > 0;
+		}
+		return false;
+	}
 });
